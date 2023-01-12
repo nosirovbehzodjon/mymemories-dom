7f6b6991ce191d08026deb32f5e700c49eb9d442
nosirovbehzodjon <behzodnosirovofficial@gmail.com>
2023-01-12 21:47:44 +0500

    wait

---
+++ index.js
@@ -0,0 +1,26 @@
+const users = [
+    {
+        name: "Behzod",
+        email: "behzodnosirovofficial@gmail.com",
+        username: "nosirovbehzodjon",
+        password: "behzod_01",
+        data: [
+            {
+                title: "Shamollarda qolgan hislarim",
+                description:
+                    "ipsum dolor sit amet consectetur adipisicing elit. Suscipit nesciunt repudiandae consectetur officia voluptate nisi pariatur! Maxime est consequuntur nobis.",
+                url: "htttps://nimadir.uz/rew32bjkjbn43m2b4nm32.png",
+            },
+        ],
+        liked: [],
+        disliked: [],
+    },
+];
+
+/*
+
+{
+    
+},
+
+*/
